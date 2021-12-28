31739fe2efa307f941eb555e8f714dee9d41df39
DEQrmichie <65993167+DEQrmichie@users.noreply.github.com>
2021-12-28 14:21:34 -0800

    doc update

--- R/obs2km.R
+++ R/obs2km.R
@@ -1,8 +1,8 @@
 #' Matches observation location to the nearest model location
 #'
-#' Returns a vector of model kilometers (or distances) that are closest to the 
-#' observation kilometer or distance. If an observation id is equally distant 
-#' between two output kilometers the first one in the vector is used. 
+#' Returns a vector of model kilometers (or long distances) that are closest to the
+#' observation kilometer or distance. If an observation id is equally distant
+#' between two output kilometers the first one in the vector is used.
 #'
 #' @param obskm Numeric vector of the observation location (e.g. stream kilometer)
 #' @param simkm Numeric vector of the model output locations (e.g. stream kilometer)
@@ -11,11 +11,11 @@
 #'
 
 obs2km <- function(obskm, simkm) {
-  
-  # Returns a dataframe matching the observation site to the closest model 
-  # simulation kilometer. 
+
+  # Returns a dataframe matching the observation site to the closest model
+  # simulation kilometer.
   # simkm is a vector of unique model simulation kilometers
-  
+
   match_km <- sapply(obskm, function(o, s) {s[which.min(abs(s-o))]}, s = simkm)
   return(match_km)
 }
