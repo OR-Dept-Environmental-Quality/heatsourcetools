0f1c6284317b3fd288e989557c73c13b81fefe01
DEQrmichie <65993167+DEQrmichie@users.noreply.github.com>
2022-01-29 21:19:43 -0800

    update plot theme and other stuff

--- scripts/plots_sites_temperature.R
+++ scripts/plots_sites_temperature.R
@@ -1,27 +1,37 @@
 #-------------------------------------------------------------------------------
 # Import observed temperature data and heat source hourly temperature output.
-# Plot observed and model prediction (hourly and 7DADM), generate
-# goodness of fit summary statistics
+# Calculate goodness of fit summary statistics, save as xlsx.
+# Plot observed and model predictions (hourly and daily maximum) at each site
 #-------------------------------------------------------------------------------
 
 library(heatsourcetools)
 library(dplyr)
 library(lubridate)
 library(ggplot2)
+library(writexl)
 
+# Plot size for word (in)
+h <- 3.5
+w <- 6.75
+
+# Plot size for ppt (in)
+#h <- 5
+#w <- 10
+
+# Name that goes on plot
 out_name <- "Jenny_01_CCC"
 out_dir <- "C:/workspace/GitHub/heatsource-9/tests/Jenny_Creek"
 
-# Name that goes on plot
 sim_name <- "Predictions"
-obs_name <- "Observations"
-
-sim_dir <- "C:/workspace/GitHub/heatsource-9/tests/Jenny_Creek/hs7"
-obs_dir <- "C:/workspace/GitHub/heatsource-9/tests/Jenny_Creek"
-
 sim_file <- "HS7.Jenny.Crk.CCCdx200.xlsm"
+sim_dir <- "C:/workspace/GitHub/heatsource-9/tests/Jenny_Creek/hs7"
+
+
+obs_name <- "Observations"
+obs_dir <- "C:/workspace/GitHub/heatsource-9/tests/Jenny_Creek"
 obs_file <- "Jenny_Creek_observed_data.xlsx"
 
+# Stations to compare with model predictions
 obs_mlocs <- c("BXON", "BXOS", "LWRX")
 
 df.preds <- read.hs.outputs(output_dir = sim_dir, file_name = sim_file,
@@ -30,7 +40,7 @@ df.preds <- read.hs.outputs(output_dir = sim_dir, file_name = sim_file,
 
 # Read obs
 # Select only calibration temperature sites
-# Match the obs kn to the preds km
+# Match the obs km to the preds km
 df.obs <- read.obs(obs_dir = obs_dir, file_name = obs_file) %>%
   filter(Monitoring.Location.ID %in% obs_mlocs &
            Characteristic.Name %in% c("Temperature, water")) %>%
@@ -58,6 +68,9 @@ preds <- calc_7dadm(df.preds) %>%
   arrange(constituent, -model_km, datetime)
 
 df.gof <- calc_gof(preds = preds, obs = obs, tolerance = 30)
+
+write_xlsx(x = df.gof,
+           path = file.path(out_dir, paste0(out_name,"_GOF.xlsx")))
 
 df <- preds %>%
   filter(model_km %in% unique(obs$model_km)) %>%
@@ -93,30 +106,36 @@ for (i in 1:length(obs.km.lookup$model_km)) {
     scale_size_manual(values = c(1, 0.5)) +
     scale_linetype_manual(values = c("solid","solid")) +
     scale_y_continuous(limits = c(ymin, ymax)) +
-    xlab("Date") +
     scale_x_datetime(date_breaks = "5 days", date_labels = "%m-%d-%Y",
                      limits = c(xmin, xmax)) +
-    ylab("Hourly Temperature (deg-C)") +
     labs(title = p.title,
          subtitle = paste0("Model Kilometer ", skm)) +
-    theme(text = element_text(size = 10)) +
-    theme(legend.position = "bottom") +
-    theme(legend.title = element_blank())
+    theme(legend.position = "bottom",
+          legend.title = element_blank(),
+          legend.key = element_blank(),
+          panel.background = element_rect(fill = "white", colour = "black"),
+          strip.background = element_rect(fill = "white", colour = "black"),
+          panel.grid.major = element_line(colour = "lightgrey"),
+          plot.title = element_text(size = 12, hjust = 0)) +
+    xlab("Date") +
+    ylab("Hourly Temperature (deg-C)")
 
   ggsave(filename = file.path(out_dir, paste0(out_name, "_hourly_km_",skm,"_",mlocID,".png")),
          plot = p1,
-         height = 3.5,
-         width = 6.75,
+         height = h,
+         width = w,
          units = "in")
 
   # Daily Maximum Temperature
   p2 <- df %>%
     filter(model_km == skm & constituent == "Daily Maximum Temperature") %>%
-    ggplot(aes(x = datetime, y = value, colour = sim, size = sim, linetype = sim)) +
-    geom_line() +
+    ggplot(aes(x = datetime, y = value, colour = sim, size = sim, 
+               shape = sim, fill = sim)) +
+    geom_point() +
     scale_color_manual(values = c("black", "red")) +
-    scale_size_manual(values = c(1, 0.5)) +
-    scale_linetype_manual(values = c("solid","solid")) +
+    scale_fill_manual(values = c("black", "red")) +
+    scale_size_manual(values = c(2, 1.5)) +
+    scale_shape_manual(values = c(21, 21)) +
     scale_y_continuous(limits = c(ymin, ymax)) +
     xlab("Date") +
     scale_x_datetime(date_breaks = "5 days", date_labels = "%m-%d-%Y",
@@ -124,13 +143,18 @@ for (i in 1:length(obs.km.lookup$model_km)) {
     ylab("Daily Maximum Temperature (deg-C)") +
     labs(title = p.title,
          subtitle = paste0("Model Kilometer ", skm)) +
-    theme(text = element_text(size = 10)) +
-    theme(legend.position = "bottom") +
-    theme(legend.title = element_blank())
+    #theme(text = element_text(size = 10)) +
+    theme(legend.position = "bottom",
+          legend.title = element_blank(),
+          legend.key = element_blank(),
+          panel.background = element_rect(fill = "white", colour = "black"),
+          strip.background = element_rect(fill = "white", colour = "black"),
+          panel.grid.major = element_line(colour = "lightgrey"),
+          plot.title = element_text(size = 12, hjust = 0))
 
-  ggsave(filename = file.path(out_dir,paste0(out_name,"_7DADM_km_",skm,"_",mlocID,".png")),
+  ggsave(filename = file.path(out_dir,paste0(out_name,"_DailyMax_km_",skm,"_",mlocID,".png")),
          plot = p2,
-         height = 3.5,
-         width = 6.75,
+         height = h,
+         width = w,
          units = "in")
 }
